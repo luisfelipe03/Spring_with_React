d6f00f19d45df1bd9ff73501ab3ceebc26061a9e
Luis Felipe <llipeandrade34@gmail.com>
2024-02-22 08:27:47 -0300

    update backend

--- imageliteapi/src/test/java/io/github/imageliteapi/ImageliteapiApplicationTests.java
+++ imageliteapi/src/test/java/io/github/imageliteapi/ImageliteapiApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.api_imagelite;
+package io.github.imageliteapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiImageliteApplicationTests {
+class ImageliteapiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
