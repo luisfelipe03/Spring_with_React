6ae5c0780bb78d16058168053e032c5f3636aeff
Luis Felipe <llipeandrade34@gmail.com>
2024-02-22 08:19:24 -0300

    new backend

---
+++ api_imagelite/src/main/java/br/com/api_imagelite/ApiImageliteApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_imagelite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiImageliteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiImageliteApplication.class, args);
+    }
+
+}
---
+++ api_imagelite/src/test/java/br/com/api_imagelite/ApiImageliteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_imagelite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiImageliteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
