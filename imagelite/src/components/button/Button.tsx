46e5e0e8175b66965cfe505adb5fc4e3dc91ca73
Luis Felipe <llipeandrade34@gmail.com>
2024-05-20 11:13:49 -0300

    form page

--- imagelite/src/components/button/Button.tsx
+++ imagelite/src/components/button/Button.tsx
@@ -4,11 +4,12 @@ interface ButtonProps {
     style?: string;
     label?: string;
     onClick?: (event: any) => void;
+    type?: "button" | "submit" | "reset" | undefined;
 }
 
-export const Button: React.FC<ButtonProps> = ({style, onClick, label} : ButtonProps) => {
+export const Button: React.FC<ButtonProps> = ({style, onClick, label, type} : ButtonProps) => {
     return (
-        <button className={`${style} text-white px-4 py-2 rounded-lg`} onClick={onClick}>
+        <button className={`${style} text-white px-4 py-2 rounded-lg`} onClick={onClick} type={type}>
             { label }
         </button>
     );
